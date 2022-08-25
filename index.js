554ab5e129caaebb5fda519c7b25f473c1600956
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-08-24 20:33:48 -0400

    passed one test

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    // console.log (data.indexOf (5));
+    // Use the data from the response to do DOM manipulation
+  });
 }
 
 function renderBooks(books) {
